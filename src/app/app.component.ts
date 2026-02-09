787ce7c51558e02ab4e805ac4a76ebdf5f986404
Bruno Ramos Lemos <bruno.lemos@live.com>
2026-02-09 19:31:28 -0300

    Feat: creating the form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { TaskFormModalComponent } from './components/task-form-modal/task-form-modal.component';
 import { TaskCommentsModalComponent } from './components/task-comments-modal/task-comments-modal.component';
+import { DialogModule } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    HeaderComponent,
-    MainContentComponent,
-    TaskFormModalComponent,
-    TaskCommentsModalComponent,
-  ],
+  imports: [HeaderComponent, MainContentComponent, DialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/services/modal-controller.service.ts
@@ -0,0 +1,43 @@
+import { inject, Injectable } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+import { TaskFormModalComponent } from '../components/task-form-modal/task-form-modal.component';
+import { TaskCommentsModalComponent } from '../components/task-comments-modal/task-comments-modal.component';
+import { ITaskFormControls } from '../interfaces/task-form-controls.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalControllerService {
+  private readonly _dialog = inject(Dialog);
+
+  private readonly modalSizeOptions = {
+    maxWidth: '620px',
+    width: '95%',
+  };
+
+  openNewTaskModal() {
+    return this._dialog.open(TaskFormModalComponent, {
+      ...this.modalSizeOptions,
+      data: {
+        mode: 'create',
+        formValues: {
+          name: '',
+          description: '',
+        },
+      },
+    });
+  }
+
+  openEditTaskModal(formValues: ITaskFormControls) {
+    return this._dialog.open(TaskFormModalComponent, {
+      ...this.modalSizeOptions,
+      data: { mode: 'edit', formValues },
+    });
+  }
+
+  openTaskCommentsModal() {
+    return this._dialog.open(TaskCommentsModalComponent, {
+      ...this.modalSizeOptions,
+    });
+  }
+}
